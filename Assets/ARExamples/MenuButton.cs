d8a35a652658b343535552ffbda2c5ba532f0d84
Jun Tachikawa <jtachikawa@xevo.com>
2019-12-27 00:12:35 +0900

    add Menu scene, and Back button and its canvas prefab

---
+++ Assets/ARExamples/MenuButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log(name + " Button clicked.");
+        SceneManager.LoadScene(name);
+    }
+}
