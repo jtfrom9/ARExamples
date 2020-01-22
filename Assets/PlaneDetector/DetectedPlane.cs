5940c968bcbf88e730d6f742968948d2be83bae7
Jun Tachikawa <jtachikawa@xevo.com>
2020-01-22 19:20:38 +0900

    temp

---
+++ Assets/PlaneDetector/DetectedPlane.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARPlane))]
+public class DetectedPlane : MonoBehaviour
+{
+    static int global_index = 0;
+    int id;
+    ARPlane m_Plane;
+
+    void Awake()
+    {
+        id = global_index;
+        global_index++;
+        m_Plane = GetComponent<ARPlane>();
+        // Debug.Log(string.Format("Created. center={0}, extents={1}, class={2}, id={3}, state={4}",
+        //     m_Plane.center.ToString(),
+        //     m_Plane.extents.ToString(),
+        //     m_Plane.classification.ToString(),
+        //     m_Plane.trackableId,
+        //     m_Plane.trackingState));
+    }
+
+    void OnBoundaryChanged(ARPlaneBoundaryChangedEventArgs eventArgs)
+    {
+        Debug.Log(string.Format("Created. center={0}, extents={1}, class={2}, id={3}",
+            m_Plane.center.ToString(),
+            m_Plane.extents.ToString(),
+            m_Plane.classification.ToString(),
+            id));
+        Debug.Log(string.Format("localPos={0}", transform.localPosition.ToString()));
+    }
+
+    void OnEnable()
+    {
+        m_Plane.boundaryChanged += OnBoundaryChanged;
+        // UpdateVisibility();
+        OnBoundaryChanged(default(ARPlaneBoundaryChangedEventArgs));
+    }
+
+    void OnDisable()
+    {
+        m_Plane.boundaryChanged -= OnBoundaryChanged;
+        // UpdateVisibility();
+    }
+
+}
---
+++ Assets/PlaneDetector/PlaneDetector.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.UI;
+using System.Linq;
+
+public class PlaneDetector : MonoBehaviour
+{
+    public ARSessionOrigin origin;
+    public Text messageTextBox;
+
+    int index = 0;
+    Dictionary<ARPlane, int> planes = new Dictionary<ARPlane, int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var planeManager = origin.GetComponent<ARPlaneManager>();
+        planeManager.planesChanged += onPlaneChanged;
+    }
+
+    void onPlaneChanged(ARPlanesChangedEventArgs args)
+    {
+        foreach (var p in args.added)
+        {
+            planes.Add(p, index);
+            Debug.Log("added: " + index + "(" + p.ToString() + ")");
+            index++;
+        }
+        foreach (var p in args.removed)
+        {
+            Debug.Log("removed: " + planes[p]);
+            planes.Remove(p);
+        }
+        foreach (var p in args.updated)
+        {
+            Debug.Log("updated: " + planes[p] + "  | " + string.Join(",", from vec in p.boundary select vec.ToString()));
+        }
+    }
+
+    void Update()
+    {
+        var planeManager = origin.GetComponent<ARPlaneManager>();
+        messageTextBox.text = string.Format("Planes: {0}", planeManager.trackables.count);
+    }
+}
